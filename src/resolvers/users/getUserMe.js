04d21cc5c70cbe45debae8a60f48d453fb9af799
25LP7668 25LP7668 <25LP7668@Tuvshins-iMac.local>
2025-11-28 12:23:08 +0800

    deploy class

--- src/resolvers/users/getUserMe.js
+++ src/resolvers/users/getUserMe.js
@@ -1,8 +1,14 @@
 import { userModel } from "../../model/user-model.js";
 import jwt from "jsonwebtoken";
-export const getMe = async (req, res) => {
+export const getUserMe = async (req, res) => {
   const token = req.headers.authorization;
-  const { id } = jwt.verify(token, "secret-key");
-  const users = await userModel.find({ _id: id });
-  res.status(200).json(users);
+  try {
+    const { id } = jwt.verify(token, "secret-key");
+    const users = await userModel.findById(id);
+    res
+      .status(200)
+      .json({ message: "User found successfully", user: users, _id: id });
+  } catch (err) {
+    res.status(401).json({ message: "Unauthorized", err });
+  }
 };
